c10e27c2167218107d21d4a25aedc249f056c9b2
phuocnehihi1 <130758054+phuocnehihi1@users.noreply.github.com>
2023-05-23 13:00:19 +0700

    Render Để Đổ Giữ Liệu và Show Để Hiện Thi  CPN

--- installBootrap/src/app/app.component.ts
+++ installBootrap/src/app/app.component.ts
@@ -9,9 +9,34 @@ import { Product } from './product/product';
 })
 export class AppComponent {
   title = 'installBootrap';
-  myProduct : Product = {
-    name: 'Hat',
-    quatity: 200,
-    price: 500
+  products : Product [] = [
+    { id:1,name:'hat', quatity:100,price:300, discount:10 },
+    { id:2,name:'T-shirt', quatity:100,price:300, discount:20 },
+    { id:3,name:'Jean', quatity:100,price:100, discount:10 }
+  ]
+
+  // Hàm Show Để Hiện Thị lên component
+  show(innerHTML:string){
+
+      let item = document.querySelector('#item');
+      if(item){
+        item.innerHTML = innerHTML;
+      }
   }
+
+// Khai Báo Hàm Render Để Gọi Dữ Liệu Vào
+render(callback: (data:string) => any){
+    let innerHTML = this.products.map(item =>{
+        return `
+            <tr>
+              <id>${item.id}</id>
+              <id>${item.name}</id>
+              <id>${item.quatity}</id>
+              <id>${item.price}</id>
+              <id>${item.discount}</id>
+            </tr> `
+    }).join('')
+  callback(innerHTML);
+  }
+
 }
